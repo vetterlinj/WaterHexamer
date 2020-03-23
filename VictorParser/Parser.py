d3300f747c643c42b1257f78b00375a2f8c20859
Nicholas Vetterli <vetterli@uw.edu>
2020-03-23 15:24:22 -0700

    Added lines for slighly different all H and all D cage and prism

--- VictorParser/Parser.py
+++ VictorParser/Parser.py
@@ -11,18 +11,21 @@ read_float = lambda datag: get_float(datag.read(8))
 read_float_block = lambda data, nblocks: np.frombuffer(data.read(8*nblocks), dtype=float)
 #fstrings
 #list of files
-#files=['d2o6_book','h2o6_prism','d2o6_prism','d2o6_cage','h2o6_cage','h2o_d2o5_book','h2o_d2o5_prism','h2o_d2o5_cage','h2o5_d2o_book','h2o5_d2o_prism','h2o5_d2o_cage','h2o6_book','sample']
+files=['h2o6_book','h2o_d2o5_book','h2o_d2o5_prism','h2o_d2o5_cage','h2o5_d2o_book','h2o5_d2o_prism','h2o5_d2o_cage','d2o6_book','sample']
+#Use above for above files, and below for below. Additional zero padding in below means they must be parsed differently.
+#REMEMBER TO USE ADDITIONAL LINE BELOW
+#files=['h2o6_prism','d2o6_prism','d2o6_cage','h2o6_cage']
 #'d2o6_prism','d2o6_cage','h2o6_prism','h2o6_cage',
-files=['sample']
+#files=['sample']
 #numberofsets=1
 
 #numbersum=0
 for file in files:
     count = 0
-#    data = open(f"NickFiles/VictorData/{file}/{file}_coords.dat", "rb")
-#     weightfile = open(f"NickFiles/VictorData/{file}/{file}_weight.dat", "r")
-    data = open(f"{file}_coords.dat", "rb")
-    weightfile = open(f"{file}_weight.dat", "r")
+    data = open(f"NickFiles/VictorData/{file}/{file}_coords.dat", "rb")
+    weightfile = open(f"NickFiles/VictorData/{file}/{file}_weight.dat", "r")
+#     data = open(f"{file}_coords.dat", "rb")
+#     weightfile = open(f"{file}_weight.dat", "r")
     skip_ws(data)
     numberofsets = read_int(data)
     print(file)
@@ -51,6 +54,8 @@ for file in files:
             read_int(data)
             read_int(data)
             wfns.append(read_float_block(data, 54))
+            #ONLY USE BELOW LINE FOR BOTTOM HOMO CAGE AND PRISM, extra zero padding
+            #read_float(data)
         wfns=np.array(wfns)
         newwfns=np.reshape(wfns,(number,18,3))
         # wfnsx=wfns[:,0::3]
@@ -64,6 +69,8 @@ for file in files:
         weightfile.readline()
         weights=[]
         print('weightstime')
+        if file == 'h2o6_prism':
+            print('oranges')
         for x in range(0, number):
             weights.append(weightfile.readline())
             weights[x] = weights[x].strip()
@@ -71,8 +78,8 @@ for file in files:
 
 #        numbersum=numbersum+number
 
-        np.savez(f'PythonData/{file}{str(count)}',coords=newwfns,weights=weights,time=time,NumWalkers=number,InitialWalkers=initialwalkers)
-        #np.savez(f'NickFiles/VictorData/{file}/PythonData/{file}{str(count)}',coords=newwfns,weights=weights,time=time,NumWalkers=number,InitialWalkers=initialwalkers)
+#        np.savez(f'PythonData/{file}{str(count)}',coords=newwfns,weights=weights,time=time,NumWalkers=number,InitialWalkers=initialwalkers)
+        np.savez(f'NickFiles/VictorData/{file}/PythonData/{file}{str(count)}',coords=newwfns,weights=weights,time=time,NumWalkers=number,InitialWalkers=initialwalkers)
         print(count)
         #weightsname='SampleCoords/'+str(count)+'NumWalkersWeights'+str(number)
         #np.save(weightsname, weights)
