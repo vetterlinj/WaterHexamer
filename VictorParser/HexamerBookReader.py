ff82571a817840db066cd259bd6219d16e9353d0
Nicholas Vetterli <vetterli@uw.edu>
2020-04-02 15:22:43 -0700

    Created the book reader. Needs optimized geometries to function properly based on assumptions.

--- VictorParser/HexamerBookReader.py
+++ VictorParser/HexamerBookReader.py
@@ -43,24 +43,75 @@ def whichoxygenisnext(walkercoords, currentwater):
         listh2.append(np.sqrt(np.sum(np.square(walkercoords[currentwater + 2] - walkercoords[i]))))
     sorth1=np.argsort(listh1)[1]
     sorth2=np.argsort(listh2)[1]
-    print(listh1)
-    print(listh2)
-    print(sorth1)
-    print(sorth2)
-    if listh1[sorth1] < listh2[sorth2]:
+    if currentwater==0:
         return sorth1*3,1
+    elif listh1[sorth1] < listh2[sorth2]:
+        if currentwater-3==sorth1*3:
+            print('thefirstone')
+            return sorth2*3,2
+        else:
+            return sorth1*3,1
     elif listh2[sorth2] < listh1[sorth1]:
-        return sorth2*3,2
+        if currentwater-3==sorth2*3:
+            print('thesecondone')
+            return sorth1*3,1
+        else:
+            return sorth2*3,2
     else:
         print('problem found')
         print(a)
+def initialoxygen(coords):
+    #find  OH distances, determine which are shortest
+    oxygen = [coords[0, :], coords[3, :], coords[6, :], coords[9, :], coords[12, :], coords[15, :]]
+    # Oxygen is (6,walkers,3)
+    h1 = [coords[1, :], coords[4, :], coords[7, :], coords[10, :], coords[13, :], coords[16, :]]
+    h2 = [coords[2, :], coords[5, :], coords[8, :], coords[11, :], coords[14, :], coords[17, :]]
+    h1 = np.array(h1)
+    h2 = np.array(h2)
+    oxygen = np.array(oxygen)
+    distances=[]
+    for hydrogenatom in np.arange(0,6):
+        listh1=[]
+        listh2=[]
+        for oxygenatom in np.arange(0,6):
+            listh1.append(np.sqrt(np.sum(np.square(h1[hydrogenatom]-oxygen[oxygenatom]))))
+            listh2.append(np.sqrt(np.sum(np.square(h2[hydrogenatom]-oxygen[oxygenatom]))))
+        distances.append(listh1)
+        distances.append(listh2)
+    distances=np.array(distances)
+    sorted=np.sort(distances)
+    bondeddistances=[]
+    for i in np.arange(0,6):
+        list=[]
+        list.append(sorted[i*2,1])
+        list.append(sorted[i*2+1,1])
+        bondeddistances.append(list)
+    bondeddistances=np.array(bondeddistances)
+    sortedbondeddistances=np.sort(bondeddistances)
+    print(distances)
 
 
-for a in np.arange(3,4):
-    candidates=oxygendistance(coords[a])
+    # roh1 = np.square(oxygen - h1)
+    # roh1 = np.sqrt(np.sum(roh1, 1))
+    # print(roh1)
+    # # roh is (6,walkers)
+    # roh2 = np.square(oxygen - h2)
+    # roh2 = np.sqrt(np.sum(roh2, 1))
+    # print(roh2)
+    # print('this is an orange')
+    return [12,0]
+
+for a in np.arange(43,1000):
+    print(coords[a]*0.529177249)
+    exit()
+    # candidates=oxygendistance(coords[a])
+    candidates=initialoxygen(coords[a])
+    exit()
     c1=candidates[0]
     c2=candidates[1]
+    # print(coords[a])
     theone, theonehydrogen=whichisitthough(coords[a],c1,c2)
+    print(f"the first water is {theone}")
     #swaps the 0th row with what we want to be oxygen 0
     coords[a][[0,theone]] = coords[a][[theone,0]]
     #ensures that the second hydrogen (coords[2]) is the one pointing to the middle, so that hydrogen 1 points to the next oxygen in the cycle.
@@ -74,8 +125,7 @@ for a in np.arange(3,4):
         nextoxygen, changehydrogen=whichoxygenisnext(coords[a],w)
         if changehydrogen==2 and w!=0:
             coords[a][[w+1, w+2]] = coords[a][[w+2, w+1]]
-        print(nextoxygen)
-        print(w)
+        print(f"the current water is {w} and the next water is {nextoxygen}")
         if nextoxygen < w:
             print('problem: switching with locked in waters')
             print(a)
